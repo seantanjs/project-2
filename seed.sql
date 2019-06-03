2576129c20c4e33113f1f4dcac72caf87aacc187
seantanjs <seantanjuisiang@gmail.com>
2019-06-03 22:10:42 +0800

    edit seed data

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
-INSERT INTO finances
-(transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
-VALUES
-('2019-06-02', 5000, 3000, 1500, 500, 1);
+-- INSERT INTO finances
+-- (transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
+-- VALUES
+-- ('2019-06-02', 5000, 3000, 1500, 500, 1);
 
 INSERT INTO finances
 (transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
