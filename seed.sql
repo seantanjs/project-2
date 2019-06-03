28ccb6b23218f57019164be650c538631a51dcd8
seantanjs <seantanjuisiang@gmail.com>
2019-06-03 22:05:21 +0800

    edited script start in package.json and edited seed file

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
--- INSERT INTO finances
--- (transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
--- VALUES
--- ('2019-06-02', 5000, 3000, 1500, 500, 1);
+INSERT INTO finances
+(transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
+VALUES
+('2019-06-02', 5000, 3000, 1500, 500, 1);
 
 INSERT INTO finances
 (transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
@@ -26,7 +26,7 @@ VALUES
 ('2019-10-02', 5000, 2500, 2200, 300, 1);
 
 
--- INSERT INTO finances
--- (transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
--- VALUES
--- ('2019-09-23', 5000, 1500, 2500, 1000, 1);
+INSERT INTO finances
+(transaction_date, monthly_income_amount, monthly_expenses_amount, monthly_investment_amount, monthly_saving_amount, user_id )
+VALUES
+('2019-11-02', 5000, 1500, 2500, 1000, 1);
